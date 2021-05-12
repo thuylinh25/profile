201bcdfa3a4565fe92c92907deeb2a25e619094f
thuylinh <lethuylinh25@gmail.com>
2021-05-12 17:30:11 +0700

    first commit

--- Pearson correlation.py
+++ Pearson correlation.py
@@ -7,7 +7,6 @@ print('Pearsons correlation:%.3f' % corr)
 
 # Cach 2
 import math
-import numpy as np
 
 physics_scores =[15,12,8,8,7,7,7,6,5,3]
 history_scores = [10,25,17,11,13,17,20,13,9,15]
@@ -15,12 +14,13 @@ history_scores = [10,25,17,11,13,17,20,13,9,15]
 def covariance(a, b):
     if len(a) != len(b):
         return
-    a_mean = np.mean(a)
-    b_mean = np.mean(b)
-    sum = 0
+    a_mean = sum(a)/float(len(a))
+    b_mean = sum(b)/float(len(b))
+    sub = 0
     for i in range(0, len(a)):
-        sum += ((a[i] - a_mean) * (b[i] - b_mean))
-    return sum/(len(a)-1)
+        sub += ((a[i] - a_mean) * (b[i] - b_mean))
+    return sub/(len(a)-1)
+
 
 cov = covariance(physics_scores, history_scores)
 
