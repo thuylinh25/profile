234a9c7711a28e998cf7c67faee5515942da37bf
thuylinh25 <70360160+thuylinh25@users.noreply.github.com>
2021-06-24 12:08:23 +0700

    Add files via upload

---
+++ Correlation and Regression Lines - A Quick Recap #2.py
@@ -0,0 +1,33 @@
+import math
+
+physics_scores =[15,12,8,8,7,7,7,6,5,3]
+history_scores = [10,25,17,11,13,17,20,13,9,15]
+
+def covariance(a, b):
+    if len(a) != len(b):
+        return
+    a_mean = sum(a)/float(len(a))
+    b_mean = sum(b)/float(len(b))
+    sub = 0
+    for i in range(0, len(a)):
+        sub += ((a[i] - a_mean) * (b[i] - b_mean))
+    return sub/(len(a)-1)
+
+
+cov = covariance(physics_scores, history_scores)
+
+def standard_deviation(data):
+    n = len(data)
+    mean = sum(data) / n
+    deviations = [(x - mean) ** 2 for x in data]
+    variance = sum(deviations) / (n-1)
+    standard_deviation = math.sqrt(variance)
+    return standard_deviation
+
+physics_std = standard_deviation(physics_scores)
+history_std = standard_deviation(history_scores)
+r = cov/(physics_std * history_std)
+# print('Pearsons correlation:%.3f' % r)
+
+m = r*(history_std/physics_std)
+print('%.3f'%m)
